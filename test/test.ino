a685ae0d26b2a395e180a7ba17d3b9976951ef08
atian722 <ayt2@rice.edu>
2025-01-29 15:02:16 -0600

    add display

--- test/test.ino
+++ test/test.ino
@@ -3,10 +3,13 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   // put your setup code here, to run once:
-
+  lcd.begin(16,2);
+  lcd.print("My name is:");
+  lcd.setCursor(0, 1);
+  lcd.print("Alice");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(50)
 }
